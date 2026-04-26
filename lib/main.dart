78553218a37ae230bc9c6d7831de78f666782e10
OldLouis <18550162163@163.com>
2026-04-26 16:26:23 +1200

    Add device preview phone frame for web

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,16 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/foundation.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: kIsWeb,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +22,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'AI Image Generator',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
